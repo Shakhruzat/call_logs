6b641d044bfc7af1a0dc5cf99b1e21938b6e6bcf
Shakhruzat <abduhalimova.shahruzat@gmail.com>
2022-05-05 12:41:23 +0300

    les 81 styles

--- lib/recent_calls/call_item.dart
+++ lib/recent_calls/call_item.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../styles/colors.dart';
+import '../styles/text_styles.dart';
+
 class CallCard extends StatefulWidget {
   const CallCard({Key? key}) : super(key: key);
 
@@ -19,28 +22,33 @@ class _CallCardState extends State<CallCard> {
             padding: EdgeInsets.only(left: 14, top: 15, right: 14, bottom: 31),
             child: Icon(
               Icons.phone_forwarded,
-              color: Color(0xFFC4C4C6),
+              color: AppColor.tertiary,
+              //Color(0xFFC4C4C6),
               size: 14,
             ),
           ),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
+              children:[
                 Text('+0 (000) 000-00-00',
-                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
-                Text('Доп. информация', style: //AppTextStyle.regular15()),
-                TextStyle(color: Color(0xff8A8A8D), fontSize: 15)),
+                    style: AppTextStyle.bold17()),
+                      // TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+                Text('Доп. информация', style: AppTextStyle.regular15()),
+                //TextStyle(color:AppColor.secondary, //Color(0xff8A8A8D),
+                    //fontSize: 15)),
               ],
             ),
           ),
-          const Text('Дата', style: TextStyle(color: Color(0xff8A8A8D), fontSize: 15)),
+           Text('Дата', style: AppTextStyle.regular15()),
+              //TextStyle(color: AppColor.secondary,//Color(0xff8A8A8D),
+              //fontSize: 15)),
           IconButton(
               padding: const EdgeInsets.only(left: 8, right: 21),
               onPressed: () => Navigator.pushNamed(context, '/info'),
               icon: const Icon(
                 Icons.info_outlined,
-                color: Color(0xff3478f6),
+                color:AppColor.link, //Color(0xff3478f6),
                 size: 22,
               )),
         ],
