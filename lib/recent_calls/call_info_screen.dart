7b56f7228371a83783cb3560167200e7c3e757d7
Shakhruzat <abduhalimova.shahruzat@gmail.com>
2022-05-20 06:34:02 +0300

    les 86

--- lib/recent_calls/call_info_screen.dart
+++ lib/recent_calls/call_info_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
 
 class CallInfo extends StatelessWidget {
   const CallInfo({Key? key}) : super(key: key);
@@ -9,8 +10,20 @@ class CallInfo extends StatelessWidget {
       body: Container(
         color: Colors.orange[300],
         alignment: Alignment.center,
-        child: const Text("Абдухалимова Шахрузат"),
+        child: FutureBuilder<http.Response>(
+          future: http.get(Uri.parse('https://catfact.ninja/fact')),
+          builder: (BuildContext context, AsyncSnapshot<http.Response>snapshot){
+            if (snapshot.hasData){
+              return Text(snapshot.data!.body);
+            } else{
+              return const CircularProgressIndicator();
+            }
+
+          }
+        )
+        //const Text("Абдухалимова Шахрузат"),
         //const Text("СКОРО БУДЕТ"),
+
       ),
     );
   }
